a833b1bafc70118b621bb6ee275e0e18781f2d45
Romain <romain.herault99@gmail.com>
2022-01-22 10:53:38 +0100

    test: registration controller

---
+++ tests/RegistrationControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    public function testRegisterForm(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/register');
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm('Register', [
+            'registration_form[username]' => 'john_doe',
+            'registration_form[email]' => 'john@doe.fr',
+            'registration_form[name]' => 'John',
+            'registration_form[plainPassword]' => 'johndoe1234',
+        ]);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorTextContains('h1', 'Homepage');
+
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy(['username' => 'john_doe']);
+
+        $this->assertEquals('john@doe.fr', $user->getEmail());
+        $this->assertEquals('John', $user->getName());
+        $this->assertNotTrue($user->isVerified());
+    }
+}
