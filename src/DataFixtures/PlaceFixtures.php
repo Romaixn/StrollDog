97d1c2d8f45fe6b983d7ec01b5901cb9cde2039e
Romain <romain.herault99@gmail.com>
2022-01-25 00:09:51 +0100

    feat: adding fixtures

---
+++ src/DataFixtures/PlaceFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\Place;
+use App\Entity\Type;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class PlaceFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        $ville = (new Type())
+            ->setName('Ville');
+        $manager->persist($ville);
+
+        $foret = (new Type())
+            ->setName('Foret');
+        $manager->persist($foret);
+
+        $champs = (new Type())
+            ->setName('Champs');
+        $manager->persist($champs);
+
+        for ($i=0; $i < 100; $i++) {
+            $place = (new Place())
+                ->setTitle($faker->text(10))
+                ->setDescription($faker->realText())
+                ->setAddress($faker->streetAddress())
+                ->setCity($faker->city())
+                ->setPostalCode($faker->departmentNumber())
+                ->setCountry($faker->country())
+                ->setLongitude($faker->longitude())
+                ->setLatitude($faker->latitude())
+                ->setRatings($faker->randomFloat(2, 0, 5))
+                ->setInflux($faker->randomElement(['Peu de monde', 'Modéré', 'Beaucoup de monde']))
+                ->addType($faker->randomElement([$ville, $foret, $champs]))
+            ;
+
+            $manager->persist($place);
+        }
+
+        $manager->flush();
+    }
+}
--- src/EventListener/PlaceListener.php
+++ src/EventListener/PlaceListener.php
@@ -23,19 +23,23 @@ class PlaceListener implements EventSubscriberInterface
 
     public function postPersist(LifecycleEventArgs $args): void
     {
-        $this->addGeo($args);
+        $this->addGeo($args, 'create');
     }
 
     public function postUpdate(LifecycleEventArgs $args): void
     {
-        $this->addGeo($args);
+        $this->addGeo($args, 'update');
     }
 
-    private function addGeo(LifecycleEventArgs $args): void
+    private function addGeo(LifecycleEventArgs $args, string $context): void
     {
         $entity = $args->getObject();
 
         if (!$entity instanceof Place) {
+            return;
+        }
+
+        if ($context === 'create' && (!empty($entity->getLongitude) || !empty($entity->getLatitude()))) {
             return;
         }
 
