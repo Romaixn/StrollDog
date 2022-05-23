8b0389d294080f1128401a32231b70951162c846
Romain <romain.herault99@gmail.com>
2022-05-23 16:34:04 +0200

    fix(security): namespace in fixtures

--- src/Domain/Security/DataFixtures/UserFixtures.php
+++ src/Domain/Security/DataFixtures/UserFixtures.php
@@ -3,7 +3,7 @@
 namespace App\Domain\Security\DataFixtures;
 
 use Faker;
-use App\Domain\Security\User;
+use App\Domain\Security\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
