295a43e6bc79af9327782c5d2c0e34d660ccf635
Romain <romain.herault99@gmail.com>
2022-05-31 16:26:52 +0200

    WIP: clean some code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
-use Rompetomp\InertiaBundle\Service\InertiaInterface;
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Rompetomp\InertiaBundle\Service\InertiaInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
